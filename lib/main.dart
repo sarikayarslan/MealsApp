74868cd41f9674a2508d9efd9f057bf2470f19e9
Arslan Sarıkaya <sarikayarslan@users.noreply.github.com>
2021-10-24 00:27:31 +0300

    affordabilityComplexityText

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'categories_screen.dart';
+import 'screens/category_meals_screen.dart';
+import 'screens/categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +14,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CategoriesScreen(),
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold
+              ))),
+      //home: CategoriesScreen(),
+      initialRoute: '/', //default is '/'
+      routes:{
+        '/':(ctx) =>CategoriesScreen(),
+        CategoryMealsScreen.routeName:(ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -40,5 +54,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
